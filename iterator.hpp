8922f5ff9aa516595e5e5919b89283538d9dafe9
Raynei <raynei@protonmail.com>
2022-01-02 09:15:55 -0500

    Fixed return type of iterator class

--- iterator.hpp
+++ iterator.hpp
@@ -1,6 +1,8 @@
 #ifndef ITERATOR_HPP
 #define ITERATOR_HPP
 
+#include <stdexcept>
+
 template <typename T>
 class Iterator {
    private:
@@ -11,42 +13,79 @@ class Iterator {
     Iterator(T* target);
     ~Iterator();
 
-    void operator++();
-    void operator+(const unsigned int moveLength);
-    void operator--();
-    void operator-(const unsigned int moveLength);
+    T& data();
     T& operator*();
+
+    Iterator<T>& operator++();
+    Iterator<T> operator++(int);
+    Iterator<T> operator+(const unsigned int moveLength);
+    Iterator<T>& operator--();
+    Iterator<T> operator--(int);
+    Iterator<T> operator-(const unsigned int moveLength);
+
+    bool operator!=(const Iterator<T>& other) const;
+    bool operator==(const Iterator<T>& other) const;
+    bool operator<(const Iterator<T>& other) const;
+    bool operator<=(const Iterator<T>& other) const;
+    bool operator>(const Iterator<T>& other) const;
+    bool operator>=(const Iterator<T>& other) const;
+};
+
+template <typename T>
+class ConstIterator : Iterator<T> {
+   private:
+    T* const ptr;
 };
 
 template <typename T>
 Iterator<T>::Iterator() : ptr(nullptr) {}
 
 template <typename T>
-Iterator<T>::Iterator(T* target) : ptr(target) {}
-
-template <typename T>
-Iterator<T>::~Iterator() {
-    delete ptr;
+Iterator<T>::Iterator(T* target) : ptr(target) {
+    if (ptr == nullptr) {
+        throw std::bad_alloc();
+    }
 }
 
 template <typename T>
-void Iterator<T>::operator++() {
-ptr++;
+Iterator<T>::~Iterator() = default;
+
+template <typename T>
+Iterator<T>& Iterator<T>::operator++() {
+    ptr++;
+    return *this;
 }
 
 template <typename T>
-void Iterator<T>::operator+(const unsigned int moveLength) {
-    ptr += moveLength;
+Iterator<T> Iterator<T>::operator++(int) {
+    Iterator<T> temp = *this;
+    ptr++;
+    return *temp;
 }
 
 template <typename T>
-void Iterator<T>::operator--() {
-ptr--;
+Iterator<T> Iterator<T>::operator+(const unsigned int moveLength) {
+    Iterator<T> temp (ptr + moveLength);
+    return temp;
 }
 
 template <typename T>
-void Iterator<T>::operator-(const unsigned int moveLength) {
-   return ptr -= moveLength;
+Iterator<T>& Iterator<T>::operator--() {
+    ptr--;
+    return *this;
+}
+
+template <typename T>
+Iterator<T> Iterator<T>::operator--(int) {
+    Iterator<T> temp = *this;
+    ptr++;
+    return *temp;
+}
+
+template <typename T>
+Iterator<T> Iterator<T>::operator-(const unsigned int moveLength) {
+    Iterator<T> temp (ptr - moveLength);
+    return temp;
 }
 
 template <typename T>
@@ -54,4 +93,38 @@ T& Iterator<T>::operator*() {
     return *ptr;
 }
 
+template <typename T>
+bool Iterator<T>::operator!=(const Iterator<T>& other) const {
+    return ptr != other.ptr;
+}
+
+template <typename T>
+bool Iterator<T>::operator==(const Iterator<T>& other) const {
+    return ptr == other.ptr;
+}
+
+template <typename T>
+bool Iterator<T>::operator<(const Iterator<T>& other) const {
+    return ptr < other.ptr;
+}
+
+template <typename T>
+bool Iterator<T>::operator<=(const Iterator<T>& other) const {
+    return ptr <= other.ptr;
+}
+
+template <typename T>
+bool Iterator<T>::operator>(const Iterator<T>& other) const {
+    return ptr > other.ptr;
+}
+
+template <typename T>
+bool Iterator<T>::operator>=(const Iterator<T>& other) const {
+    return ptr >= other.ptr;
+}
+
+template <typename T>
+T& Iterator<T>::data() {
+    return *ptr;
+}
 #endif  // ITERATOR_HPP
