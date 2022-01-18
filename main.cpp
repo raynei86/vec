e9cf159ccd729b41d5f1b2c4acf8b9359054ecb5
Raynei <raynei@protonmail.com>
2022-01-17 22:45:14 -0500

    Added a "test"

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include <string>
+#include "iterator.hpp"
+#include "vec.hpp"
+
+int main() {
+    std::ios::sync_with_stdio(0);
+
+    Vec<int> test = {1, 2, 3, 4, 5};
+    std::cout << "Inital vector: ";
+    for (std::size_t it = 0; it < test.size(); it++) {
+        std::cout << "[" << test[it] << "] ";
+    }
+
+    std::cout << "\n----------\n";
+
+    test.push_back(6);
+    test.push_back(7);
+    test.push_back(8);
+    std::cout << "Push back test: ";
+    for (std::size_t it = 0; it < test.size(); it++) {
+        std::cout << "[" << test[it] << "] ";
+    }
+
+    std::cout << "\n----------\n";
+
+    test.pop_back();
+    std::cout << "Pop back test: ";
+    for (std::size_t it = 0; it < test.size(); it++) {
+        std::cout << "[" << test[it] << "] ";
+    }
+
+    std::cout << "\n----------\n";
+
+    Vec<int> swapTest;
+    test.swap(swapTest);
+    std::cout << "Swap test: ";
+    for (std::size_t it = 0; it < swapTest.size(); it++) {
+        std::cout << "[" << swapTest[it] << "] ";
+    }
+    swapTest.swap(test);
+
+    std::cout << "\n----------\n";
+
+    test.erase(test.begin());
+    std::cout << "Erase test: ";
+    for (std::size_t it = 0; it < test.size(); it++) {
+        std::cout << "[" << test[it] << "] ";
+    }
+
+    std::cout << "\n----------\n";
+
+    test.resize(5);
+    std::cout << "Resize test: ";
+    for (std::size_t it = 0; it < test.size(); it++) {
+        std::cout << "[" << test[it] << "] ";
+    }
+
+    std::cout << "\n----------\n";
+
+    test.insert(test.begin(), 1);
+    std::cout << "Insert test: ";
+    for (std::size_t it = 0; it < test.size(); it++) {
+        std::cout << "[" << test[it] << "] ";
+    }
+    std::cout << "\n----------\n";
+    std::cout << "Test done.\n";
+    return 0;
+}
